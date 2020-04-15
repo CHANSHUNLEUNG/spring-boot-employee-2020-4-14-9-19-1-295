374daa47018e478c97fc8f19360eccfbd070434e
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-15 18:38:39 +0800

    Add api for update employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.entity.Employee;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,19 +12,38 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
     List<Employee> employees = new ArrayList<>();
-    @GetMapping
-    public List<Employee> getEmployees(){
+
+    public EmployeeController() {
         employees.add(new Employee(1,"leo1",18, "male"));
         employees.add(new Employee(2,"leo2",18, "male"));
         employees.add(new Employee(3,"leo3",18, "male"));
         employees.add(new Employee(4,"leo4",18, "male"));
+    }
+
+    @GetMapping
+    public List<Employee> getEmployees(){
         return employees;
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployees(@RequestBody Employee newEmployee){
         employees.add(newEmployee);
         return newEmployee;
     }
+    @PutMapping
+    public ResponseEntity<Object> updateEmployees(@RequestBody Employee newEmployee){
+        Employee targetEmployee = this.employees.stream()
+                .filter(employee -> employee.getId() == newEmployee.getId())
+                .findFirst()
+                .orElse(null);
+        if(targetEmployee == null){
+            return new ResponseEntity<>("Employee does not exist",HttpStatus.BAD_REQUEST);
+        }
+        employees.set(employees.indexOf(targetEmployee),newEmployee);
+        return new ResponseEntity<>(newEmployee,HttpStatus.OK);
+    }
+
+
 
 }
