e2c0e41386b94d189605e9856320cc4a9f25ec79
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-15 20:31:47 +0800

    Add api get employees in a company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,6 +17,9 @@ public class CompanyController {
         this.companies.add(new Company(2,"company2",100));
         this.companies.add(new Company(3,"company3",100));
         this.companies.add(new Company(4,"company4",100));
+        this.companies.get(0).getEmployees().add(new Employee(1,"leo1",18,"male",80000));
+        this.companies.get(0).getEmployees().add(new Employee(2,"leo2",18,"male",80000));
+        this.companies.get(0).getEmployees().add(new Employee(3,"leo3",18,"male",80000));
     }
     @GetMapping
     public List<Company> getCompanies(){
@@ -29,4 +33,13 @@ public class CompanyController {
                 .orElse(null);
         return returnCompany;
     }
+    @GetMapping(path = "/{companyID}/employees")
+    public List<Employee> getEmployeesInCompany(@PathVariable int companyID){
+        Company returnCompany = this.companies.stream()
+                .filter(company -> company.getId() == companyID)
+                .findFirst()
+                .orElse(null);
+        return returnCompany.getEmployees();
+    }
+
 }
