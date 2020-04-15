730b83c69a71fe3fb2be7cf7d88ec5889f769530
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-15 18:50:34 +0800

    Add api for delete employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -14,36 +14,48 @@ public class EmployeeController {
     List<Employee> employees = new ArrayList<>();
 
     public EmployeeController() {
-        employees.add(new Employee(1,"leo1",18, "male"));
-        employees.add(new Employee(2,"leo2",18, "male"));
-        employees.add(new Employee(3,"leo3",18, "male"));
-        employees.add(new Employee(4,"leo4",18, "male"));
+        employees.add(new Employee(1, "leo1", 18, "male"));
+        employees.add(new Employee(2, "leo2", 18, "male"));
+        employees.add(new Employee(3, "leo3", 18, "male"));
+        employees.add(new Employee(4, "leo4", 18, "male"));
     }
 
     @GetMapping
-    public List<Employee> getEmployees(){
+    public List<Employee> getEmployees() {
         return employees;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createEmployees(@RequestBody Employee newEmployee){
+    public Employee createEmployees(@RequestBody Employee newEmployee) {
         employees.add(newEmployee);
         return newEmployee;
     }
+
     @PutMapping
-    public ResponseEntity<Object> updateEmployees(@RequestBody Employee newEmployee){
+    public ResponseEntity<Object> updateEmployees(@RequestBody Employee newEmployee) {
         Employee targetEmployee = this.employees.stream()
                 .filter(employee -> employee.getId() == newEmployee.getId())
                 .findFirst()
                 .orElse(null);
-        if(targetEmployee == null){
-            return new ResponseEntity<>("Employee does not exist",HttpStatus.BAD_REQUEST);
+        if (targetEmployee == null) {
+            return new ResponseEntity<>("Error,Employee does not exist", HttpStatus.BAD_REQUEST);
         }
-        employees.set(employees.indexOf(targetEmployee),newEmployee);
-        return new ResponseEntity<>(newEmployee,HttpStatus.OK);
+        employees.set(employees.indexOf(targetEmployee), newEmployee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
     }
 
-
+    @DeleteMapping(path = "/{employeeID}")
+    public ResponseEntity<Object> updateEmployees(@PathVariable int employeeID) {
+        Employee targetEmployee = this.employees.stream()
+                .filter(employee -> employee.getId() == employeeID)
+                .findFirst()
+                .orElse(null);
+        if (targetEmployee == null) {
+            return new ResponseEntity<>("Error,Employee does not exist", HttpStatus.BAD_REQUEST);
+        }
+        employees.remove(targetEmployee);
+        return new ResponseEntity<>("Remove employee with id " + employeeID + " successfully", HttpStatus.OK);
+    }
 
 }
