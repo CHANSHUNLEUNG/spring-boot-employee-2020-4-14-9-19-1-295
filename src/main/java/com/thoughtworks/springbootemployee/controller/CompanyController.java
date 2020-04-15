041e75f9c77994c65540dd76c21308ed33a7fc6a
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-15 20:39:04 +0800

    Add api add company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -15,28 +15,31 @@ public class CompanyController {
     private List<Company> companies = new ArrayList<>();
 
     public CompanyController() {
-        this.companies.add(new Company(1,"company1",100));
-        this.companies.add(new Company(2,"company2",100));
-        this.companies.add(new Company(3,"company3",100));
-        this.companies.add(new Company(4,"company4",100));
-        this.companies.get(0).getEmployees().add(new Employee(1,"leo1",18,"male",80000));
-        this.companies.get(0).getEmployees().add(new Employee(2,"leo2",18,"male",80000));
-        this.companies.get(0).getEmployees().add(new Employee(3,"leo3",18,"male",80000));
+        this.companies.add(new Company(1, "company1", 100));
+        this.companies.add(new Company(2, "company2", 100));
+        this.companies.add(new Company(3, "company3", 100));
+        this.companies.add(new Company(4, "company4", 100));
+        this.companies.get(0).getEmployees().add(new Employee(1, "leo1", 18, "male", 80000));
+        this.companies.get(0).getEmployees().add(new Employee(2, "leo2", 18, "male", 80000));
+        this.companies.get(0).getEmployees().add(new Employee(3, "leo3", 18, "male", 80000));
     }
+
     @GetMapping
-    public List<Company> getCompanies(){
+    public List<Company> getCompanies() {
         return this.companies;
     }
+
     @GetMapping(path = "/{companyID}")
-    public Company getCompany(@PathVariable int companyID){
+    public Company getCompany(@PathVariable int companyID) {
         Company returnCompany = this.companies.stream()
                 .filter(company -> company.getId() == companyID)
                 .findFirst()
                 .orElse(null);
         return returnCompany;
     }
+
     @GetMapping(path = "/{companyID}/employees")
-    public List<Employee> getEmployeesInCompany(@PathVariable int companyID){
+    public List<Employee> getEmployeesInCompany(@PathVariable int companyID) {
         Company returnCompany = this.companies.stream()
                 .filter(company -> company.getId() == companyID)
                 .findFirst()
@@ -46,7 +49,7 @@ public class CompanyController {
 
     @GetMapping(params = {"page", "pageSize"})
     public ResponseEntity<Object> getCompanies
-            (@RequestParam(value = "page") int page,@RequestParam(value = "pageSize") int pageSize){
+            (@RequestParam(value = "page") int page, @RequestParam(value = "pageSize") int pageSize) {
         int startIndex = (page - 1) * pageSize;
         int endIndex = page * pageSize;
         if (this.companies.size() < startIndex) {
@@ -55,6 +58,11 @@ public class CompanyController {
             return new ResponseEntity<>(this.companies.subList(startIndex, companies.size()), HttpStatus.OK);
         }
         return new ResponseEntity<>(this.companies.subList((page - 1) * pageSize, page * pageSize), HttpStatus.OK);
+    }
+    @PostMapping
+    public Company addCompanies(@RequestBody Company company){
+        this.companies.add(company);
+        return company;
     }
 
 }
