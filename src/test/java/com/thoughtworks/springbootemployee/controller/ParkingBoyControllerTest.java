eeec8f9074e1e31500e910ec2c2012f84b3532f5
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-17 19:54:16 +0800

    add test case should_create_parking_boy_when_given_a_new_parking_boy

--- src/test/java/com/thoughtworks/springbootemployee/controller/ParkingBoyControllerTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/controller/ParkingBoyControllerTest.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.model.ParkingBoy;
 import com.thoughtworks.springbootemployee.repository.ParkingBoyRepository;
 import com.thoughtworks.springbootemployee.service.ParkingBoyService;
 import org.junit.Test;
@@ -21,5 +23,15 @@ public class ParkingBoyControllerTest {
         parkingBoyService.getAll();
 
         Mockito.verify(parkingBoyRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    public void should_create_parking_boy_when_given_a_new_parking_boy() {
+        ParkingBoy parkingBoy = new ParkingBoy(1,"leo",
+                new Employee(1,"leo chan",18,"male",80000,1));
+
+        parkingBoyService.addParkingBoy(parkingBoy);
+
+        Mockito.verify(parkingBoyRepository,Mockito.times(1)).save(parkingBoy);
     }
 }
