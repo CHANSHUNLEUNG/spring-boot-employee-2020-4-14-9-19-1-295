a8d154f956e76c77d15a7c8ff07781d52d2be5ee
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-20 16:31:54 +0800

    add flyway V202004201625__create_employee_table.sql, V202004201628__create_company_table.sql

---
+++ src/main/resources/db/migration/V202004201625__create_employee_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+    id int primary key,
+    name varchar(255),
+    age int,
+    gender varchar(255),
+    salary int,
+    company_id int
+)
---
+++ src/main/resources/db/migration/V202004201628__create_company_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE company (
+    id int primary key,
+    name varchar(255),
+    employees_number int
+)
